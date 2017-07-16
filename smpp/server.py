3af88d5ce850da16b67cfc9f577d24aaa32d47d1
Oleg Yamnikov <bgeyts667@gmail.com>
2017-07-16 16:09:35 +0500

    Implement connections binding and unbinding

--- smpp/server.py
+++ smpp/server.py
@@ -1,17 +1,54 @@
 import asyncio
 import struct
+from collections import defaultdict
+from enum import Enum
+
+
+class Mode(Enum):
+    UNBOUND = 0
+    RECEIVER = 1
+    TRANSMITTER = 2
+    TRANSCEIVER = 3
 
 
 class Connection:
     def __init__(self, r: asyncio.StreamReader, w: asyncio.StreamWriter):
+        self.mode = Mode.UNBOUND
+        self.system_id = None
         self.r = r
         self.w = w
+
+    def __repr__(self) -> str:
+        return "Connection({}, {})".format(self.mode, self.system_id)
 
 
 class Server:
     def __init__(self, host='0.0.0.0', port=2775):
         self.host = host
         self.port = port
+
+        # Maps system_ids to connections.
+        self._connections = defaultdict(set) # type: Dict[int, Set[Connection]]
+
+    def _bind(self, conn: Connection, m: Mode, sid: int):
+        conn.mode = m
+        conn.system_id = sid
+        self._connections[sid].add(conn)
+
+    def _unbind(self, conn: Connection):
+        conn.mode = Mode.UNBOUND
+        conn.system_id = None
+
+        remove_sids = set()
+        for sid, _conns in self._connections.items():
+            if conn in _conns:
+                _conns.remove(conn)
+
+            if len(_conns) == 0:
+                remove_sids.add(sid)
+
+        for sid in remove_sids:
+            del self._connections[sid]
 
     async def _on_client_connected(self, conn: Connection):
         try:
@@ -32,8 +69,10 @@ class Server:
 
                 print("Read pdu", len(pdu), "bytes long:", pdu)
         except asyncio.IncompleteReadError:
-            conn.w.close()
-            return
+            pass
+
+        conn.w.close()
+        self._unbind(conn)
 
 
     async def start(self):
