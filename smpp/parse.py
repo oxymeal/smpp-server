b3db24546c52975cf1c96e43fc7206215e4f1f76
DeFiXik <mr.defix@gmail.com>
2017-07-18 21:50:12 +0500

    Complete DeliverSm

--- smpp/parse.py
+++ smpp/parse.py
@@ -771,6 +771,51 @@ class SubmitMultiResp(PDU):
         return bs
 
 
+class DeliverSm(PDU):
+
+    command = Command.DELIVER_SM
+
+    def __init__(self):
+        super().__init__()
+        self.service_type = ""
+        self.source_addr_ton = 0
+        self.source_addr_npi = 0
+        self.source_addr = ""
+        self.dest_addr_ton = 0
+        self.dest_addr_npi = 0
+        self.destination_addr = ""
+
+    @property
+    def command_length(self) -> int:
+        head_size = 16
+        st_size = len(self.service_type) + 1
+        sat_size = 1
+        san_size = 1
+        sad_size = len(self.source_addr) + 1
+        dat_size = 1
+        dan_size = 1
+        da_size = len(self.destination_addr) + 1
+        return head_size + st_size + sat_size + san_size\
+                 + sad_size + dat_size + dan_size + da_size
+
+    @classmethod
+    def unpack(cls, bs: bytearray) -> 'DeliverSm':
+        pdu = DeliverSm()
+
+        pdu._unpack_header()
+        pdu.service_type, bs = unpack_coctet_string(bs)
+        (sat, san), bs = _unpack_fmt('!BB', bs)
+        pdu.source_addr_ton = sat
+        pdu.source_addr_npi = san
+        pdu.source_addr, bs = unpack_coctet_string(bs)
+        (dat, dan), bs = _unpack_fmt('!BB', bs)
+        pdu.dest_addr_ton = dat
+        pdu.dest_addr_npi = dan
+        pdu.destination_addr, _ = unpack_coctet_string(bs)
+
+        return pdu
+
+
 _COMMAND_CLASSES = {
     Command.BIND_RECEIVER: BindReceiver,
     Command.BIND_RECEIVER_RESP: BindReceiverResp,
@@ -787,6 +832,7 @@ _COMMAND_CLASSES = {
     Command.SUBMIT_SM_RESP: SubmitSmResp,
     Command.SUBMIT_MULTI: SubmitMulti,
     Command.SUBMIT_MULTI_RESP: SubmitMultiResp,
+    Command.DELIVER_SM: DeliverSm,
     # QUERY_SM = 0x00000003
     # QUERY_SM_RESP = 0x80000003
     # DELIVER_SM = 0x00000005
