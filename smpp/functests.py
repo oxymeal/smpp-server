20ec9e8f95e553b7923ef2f4e8f5d6886e6d8614
Oleg Yamnikov <bgeyts667@gmail.com>
2017-07-19 15:10:50 +0500

    Add tests for invalid bind status errors

--- smpp/functests.py
+++ smpp/functests.py
@@ -3,6 +3,7 @@ import socket
 import unittest
 import multiprocessing
 
+from smpp.vendor.smpplib import consts
 from smpp.vendor.smpplib.client import Client
 from smpp.vendor.smpplib.smpp import make_pdu
 from smpp.server import Server
@@ -51,3 +52,41 @@ class EnquireLinkTestCase(unittest.TestCase):
         self.assertEqual(resp.sequence, command.sequence)
 
         client.disconnect()
+
+class BindStatusCheckingTestCase(unittest.TestCase):
+    def setUp(self):
+        self.sproc = start_server_proc()
+
+    def tearDown(self):
+        self.sproc.terminate()
+
+    def test_nobind_nack(self):
+        client = Client('localhost', TEST_SERVER_PORT)
+        client.connect()
+
+        cmd = make_pdu('submit_sm', client=client,
+                       source_addr_ton=12, source_addr="src",
+                       dest_addr_ton=34, destination_addr="dst",
+                       short_message=b"Hello world")
+        client.send_pdu(cmd)
+        resp = client.read_pdu()
+
+        self.assertEqual(resp.command, 'generic_nack')
+        self.assertEqual(resp.sequence, cmd.sequence)
+        self.assertEqual(resp.status, consts.SMPP_ESME_RINVBNDSTS)
+
+    def test_rcv_submit_nack(self):
+        client = Client('localhost', TEST_SERVER_PORT)
+        client.connect()
+        client.bind_receiver()
+
+        cmd = make_pdu('submit_sm', client=client,
+                       source_addr_ton=12, source_addr="src",
+                       dest_addr_ton=34, destination_addr="dst",
+                       short_message=b"Hello world")
+        client.send_pdu(cmd)
+        resp = client.read_pdu()
+
+        self.assertEqual(resp.command, 'generic_nack')
+        self.assertEqual(resp.sequence, cmd.sequence)
+        self.assertEqual(resp.status, consts.SMPP_ESME_RINVBNDSTS)
