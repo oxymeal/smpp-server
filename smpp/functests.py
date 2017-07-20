f731ea21c645fec8fc6290be82cd69f2930e165c
Oleg Yamnikov <bgeyts667@gmail.com>
2017-07-20 19:45:14 +0500

    Implement a test for store and forward messaging procedure

--- smpp/functests.py
+++ smpp/functests.py
@@ -2,6 +2,7 @@ import time
 import socket
 import unittest
 import threading
+from typing import List
 
 from smpp import external
 from smpp.server import Server
@@ -185,3 +186,109 @@ class BindAuthTestCase(unittest.TestCase):
         with self.assertRaises(PDUError):
             client.bind_transmitter(
                 system_id=self.INCORRECT_SID, password=self.INCORRECT_PWD)
+
+
+class MessagingTestCase(unittest.TestCase):
+    class DummyProvider:
+        def __init__(self):
+            self.status = external.DeliveryStatus.OK
+            self.mlock = threading.Lock()
+            self.msem = threading.Semaphore(0)
+            self.messages = []
+
+        def authenticate(self, system_id: str, password: str) -> bool:
+            return True
+
+        def deliver(self, sm: external.ShortMessage) -> external.DeliveryStatus:
+            with self.mlock:
+                self.messages.append(sm)
+
+            self.msem.release()
+
+            return self.status
+
+        def wait_for_message(self, timeout=None):
+            self.msem.acquire(timeout=timeout)
+
+        def get_messages(self) -> List[external.ShortMessage]:
+            with self.mlock:
+                return self.messages
+
+    def setUp(self):
+        self.provider = self.DummyProvider()
+        self.server, self.sthread = start_server_thread(provider=self.provider)
+
+    def tearDown(self):
+        self.server.stop()
+        self.sthread.join()
+
+    def test_store_and_forward(self):
+        t = Client('localhost', TEST_SERVER_PORT, timeout=1)
+        t.connect()
+        t.bind_transmitter(system_id="mtc", password="pwd")
+
+        r1 = Client('localhost', TEST_SERVER_PORT, timeout=1)
+        r1.connect()
+        r1.bind_receiver(system_id="mtc", password="pwd")
+
+        r2 = Client('localhost', TEST_SERVER_PORT, timeout=1)
+        r2.connect()
+        r2.bind_receiver(system_id="mtc", password="pwd")
+
+        rx = Client('localhost', TEST_SERVER_PORT, timeout=1)
+        rx.connect()
+        rx.bind_receiver(system_id="nomtc", password="pwd")
+
+        ssm = t.send_message(
+            esm_class=consts.SMPP_MSGMODE_STOREFORWARD,
+            registered_delivery=0x00000001, # Request delivery receipt
+            source_addr_ton=12,
+            source_addr_npi=34,
+            source_addr="src",
+            dest_addr_ton=56,
+            dest_addr_npi=67,
+            destination_addr="dst",
+            short_message=b"Hello world!")
+
+        ssmr = t.read_pdu()
+        self.assertEqual(ssmr.command, 'submit_sm_resp')
+        self.assertEqual(ssmr.sequence, ssm.sequence)
+
+        self.provider.wait_for_message(timeout=1)
+
+        messages = self.provider.get_messages()
+        self.assertEqual(len(messages), 1)
+
+        msg = messages[0]
+        self.assertEqual(msg.system_id, "mtc")
+        self.assertEqual(msg.password, "pwd")
+        self.assertEqual(msg.source_addr_ton, 12)
+        self.assertEqual(msg.source_addr_npi, 34)
+        self.assertEqual(msg.source_addr, "src")
+        self.assertEqual(msg.dest_addr_ton, 56)
+        self.assertEqual(msg.dest_addr_npi, 67)
+        self.assertEqual(msg.destination_addr, "dst")
+        self.assertEqual(msg.body, "Hello world!")
+
+        r1rec = r1.read_pdu()
+        self.assertEqual(r1rec.command, 'deliver_sm')
+        self.assertEqual(int(r1rec.source_addr_ton), 12)
+        self.assertEqual(int(r1rec.source_addr_npi), 34)
+        self.assertEqual(r1rec.source_addr, b"src")
+        self.assertEqual(int(r1rec.dest_addr_ton), 56)
+        self.assertEqual(int(r1rec.dest_addr_npi), 67)
+        self.assertEqual(r1rec.destination_addr, b"dst")
+        self.assertEqual(r1rec.short_message, b"Hello world!")
+
+        r2rec = r2.read_pdu()
+        self.assertEqual(r1rec.command, 'deliver_sm')
+        self.assertEqual(int(r2rec.source_addr_ton), 12)
+        self.assertEqual(int(r2rec.source_addr_npi), 34)
+        self.assertEqual(r2rec.source_addr, b"src")
+        self.assertEqual(int(r2rec.dest_addr_ton), 56)
+        self.assertEqual(int(r2rec.dest_addr_npi), 67)
+        self.assertEqual(r2rec.destination_addr, b"dst")
+        self.assertEqual(r2rec.short_message, b"Hello world!")
+
+        with self.assertRaises(socket.timeout):
+            rx.read_pdu()
