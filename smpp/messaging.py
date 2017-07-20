c3016db37bc4a183c09f48cf59adb8e2a1bec873
zloi-vasya <vasya-1995@mail.ru>
2017-07-20 16:29:08 +0500

    Fix errors

--- smpp/messaging.py
+++ smpp/messaging.py
@@ -1,12 +1,15 @@
 import logging
 import uuid
 from datetime import datetime
+from datetime import timedelta
 from typing import Optional
 
 from . import external, parse
 
 
 logger = logging.getLogger(__name__)
+
+DEFAULT_VALIDITY_PERIOD = 60
 
 
 class ResponseSender:
@@ -64,13 +67,12 @@ class Dispatcher:
         """
         logger.info('Dispatching PDU {}'.format(pdu.command))
 
-
         if pdu.command == parse.Command.SUBMIT_SM:
             sm = external.base.ShortMessage(
-                self.system_id, self.password,
-                pdu.source_addr_ton, pdu.source_addr_npi, source_addr,
-                dest_addr_ton, dest_addr_npi, destination_addr,
-                body
+                system_id=self.system_id, password=self.password,
+                source_addr_ton=pdu.source_addr_ton, source_addr_npi=pdu.source_addr_npi, source_addr=pdu.source_addr,
+                dest_addr_ton=pdu.dest_addr_ton, dest_addr_npi=pdu.dest_addr_npi, destination_addr=pdu.destination_addr,
+                body=pdu.short_message
             )
 
             if pdu.esm_class == parse.STORE_AND_FORWARD: #Store and forward
@@ -83,33 +85,33 @@ class Dispatcher:
 
                 await rs.send(submit_sm_resp)
 
-                #TODO:
-                # CREATE FUNC FOR THIS TIME WORKAROUND
+                if not pdu.validity_period:
+                    sm_validity_period = datetime.now() + timedelta(0, DEFAULT_VALIDITY_PERIOD)
+                else:
+                    time_offset = pdu.validity_period[13:15]
+                    time_offset_sign = pdu.validity_period[-1]
 
-                time_offset = pdu.validity_period[13:15]
-                time_offset_sign = pdu.validity_period[-1]
+                    time_offset_in_minutes = int(time_offset) * 15
+                    time_offset_hours = time_offset_in_minutes // 60
+                    time_offset_minutes = time_offset_in_minutes - time_offset_hours * 60
 
-                time_offset_in_minutes = int(time_offset) * 15
-                time_offset_hours = time_offset_in_minutes // 60
-                time_offset_minutes = time_offset_in_minutes - time_offset_hours * 60
+                    time_offset_hours_formatted = str(time_offset_hours).rjust(2, "0")
+                    time_offset_minutes_formatted = str(time_offset_minutes).rjust(2, "0")
+                    time_offset_formatted = "".join([time_offset_sign, time_offset_hours_formatted, time_offset_minutes_formatted])
 
-                time_offset_hours_formatted = str(time_offset_hours).rjust(2, "0")
-                time_offset_minutes_formatted = str(time_offset_minutes).rjust(2, "0")
-                time_offset_formatted = "".join([time_offset_sign, time_offset_hours_formatted, time_offset_minutes_formatted])
+                    validity_period_formatted = pdu.validity_period[:12] + time_offset_formatted
 
-                validity_period_formatted = pdu.validity_period[:12] + time_offset_formatted
-
-                sm_validity_period = datetime.strptime(validity_period_formatted, "%y%m%d%H%M%S%z")
+                    sm_validity_period = datetime.strptime(validity_period_formatted, "%y%m%d%H%M%S%z")
 
                 status = ""
                 while True:
-                    sm_status = eprovider.deliver(sm)
+                    sm_status = self.eprovider.deliver(sm)
 
-                    if sm_status == external.DeliveryStatus.OK or (datetime.now() < sm_validity_period):
+                    if sm_status == external.base.DeliveryStatus.OK or (datetime.now() < sm_validity_period):
                         status = sm_status
                         break
 
-                if status == external.DeliveryStatus.OK and pdu.registered_delivery == 1:
+                if status == external.base.DeliveryStatus.OK and pdu.registered_delivery == 1:
 
                     deliver_sm = parse.DeliverSm()
                     deliver_sm.service_type = pdu.service_type
@@ -118,7 +120,7 @@ class Dispatcher:
                     deliver_sm.source_addr = pdu.source_addr
                     deliver_sm.dest_addr_ton = pdu.dest_addr_ton
                     deliver_sm.dest_addr_npi = pdu.dest_addr_npi
-                    deliver.destination_addr = pdu.destination_addr
+                    deliver_sm.destination_addr = pdu.destination_addr
                     deliver_sm.sequence_number = pdu.sequence_number
 
                     await rs.send_to_rcv(deliver_sm)
