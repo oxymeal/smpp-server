7c605973616206aa439075f2e710e417ba80d5c6
Oleg Yamnikov <bgeyts667@gmail.com>
2017-07-23 20:46:41 +0500

    Remove redeclarations of ShortMessage and DeliveryStatus from main.py

--- main.py
+++ main.py
@@ -1,36 +1,14 @@
 #!/usr/bin/env python3
 import logging
-from smpp.server import Server
 import random
-
 from enum import Enum
+
+from smpp.server import Server
+from smpp.external import ShortMessage, DeliveryStatus
+
 
 logging.basicConfig(level=logging.DEBUG)
 
-class ShortMessage:
-    def __init__(self, *,
-                 system_id: str, password: str,
-                 source_addr_ton: int, source_addr_npi :int, source_addr: str,
-                 dest_addr_ton: int, dest_addr_npi: int, destination_addr: str,
-                 body: str):
-        self.system_id = system_id
-        self.password = password
-
-        self.source_addr_ton = source_addr_ton
-        self.source_addr_npi = source_addr_npi
-        self.source_addr = source_addr
-
-        self.dest_addr_ton = dest_addr_ton
-        self.dest_addr_npi = dest_addr_npi
-        self.destination_addr = destination_addr
-
-        self.body = body
-
-class DeliveryStatus(Enum):
-    OK = 0
-    GENERIC_ERROR = 1 # Неуказанная ошибка
-    AUTH_FAILED = 2   # Неверный system_id/пароль
-    NO_BALANCE = 3    # Недостаточно средств на счете
 
 class Provider:
     """
