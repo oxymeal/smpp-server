1924c999571e8ef2d582cd4731a32fe67290cb17
zloi-vasya <vasya-1995@mail.ru>
2017-07-23 11:31:36 +0500

    Add fake sms provider for text

--- main.py
+++ main.py
@@ -1,7 +1,55 @@
 import logging
 from smpp.server import Server
+import random
+
+from enum import Enum
 
 logging.basicConfig(level=logging.DEBUG)
 
-s = Server()
+class ShortMessage:
+    def __init__(self, *,
+                 system_id: str, password: str,
+                 source_addr_ton: int, source_addr_npi :int, source_addr: str,
+                 dest_addr_ton: int, dest_addr_npi: int, destination_addr: str,
+                 body: str):
+        self.system_id = system_id
+        self.password = password
+
+        self.source_addr_ton = source_addr_ton
+        self.source_addr_npi = source_addr_npi
+        self.source_addr = source_addr
+
+        self.dest_addr_ton = dest_addr_ton
+        self.dest_addr_npi = dest_addr_npi
+        self.destination_addr = destination_addr
+
+        self.body = body
+
+class DeliveryStatus(Enum):
+    OK = 0
+    GENERIC_ERROR = 1 # Неуказанная ошибка
+    AUTH_FAILED = 2   # Неверный system_id/пароль
+    NO_BALANCE = 3    # Недостаточно средств на счете
+
+class Provider:
+    """
+    Этот класс используется как интерфейс к внешнему доставщику сообщений.
+    """
+
+    def authenticate(self, system_id: str, password: str) -> bool:
+        return True
+
+    def deliver(self, sm: ShortMessage) -> DeliveryStatus:
+        """
+        Метод выполняет попытку доставки короткого сообщения и возвращает
+        один и перечисленных в DeliveryStatus результатов отправки.
+        """
+
+        f = open('container/sms.txt', 'a')
+        f.write(str(sm.__dict__))
+        f.write("\n")
+
+        return random.choice(list(DeliveryStatus))
+
+s = Server(provider=Provider())
 s.run()
